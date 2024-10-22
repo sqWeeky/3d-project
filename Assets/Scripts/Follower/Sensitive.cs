abf3d0a28fc8fd13f8aa051c601b14e606d63d71
sqWeeky <ilya.mil99@gmail.com>
2024-10-22 21:41:00 +0500

    dghd

--- Assets/Scripts/Follower/Sensitive.cs
+++ Assets/Scripts/Follower/Sensitive.cs
@@ -1,25 +1,23 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Sensitive : MonoBehaviour
 {
+    private int _numberLayerMaskStairs = 7;
     private bool _onStairs = false;
 
     public bool OnStairs => _onStairs;
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.layer == 7)
+        if (other.gameObject.layer == _numberLayerMaskStairs)
         {
             _onStairs = true;
-        }
-        
+        }        
     }
 
     private void OnTriggerExit(Collider other)
     {
-        if (other.gameObject.layer == 7)
+        if (other.gameObject.layer == _numberLayerMaskStairs)
         {
             _onStairs = false;
         }
